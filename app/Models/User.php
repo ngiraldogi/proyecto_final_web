1798fcedfdc2a697f32b3c1d38f30672b18eebc3
dflopezlo <dflopezlo@unal.edu.co>
2022-06-27 12:49:23 -0500

    se añade la barra de navegación y la tabla usuario creadas

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,12 +11,14 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    /*
     protected $fillable = [
         'name',
         'email',
@@ -28,6 +30,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    /*
     protected $hidden = [
         'password',
         'remember_token',
@@ -38,7 +41,9 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
+    /*
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    */
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombreUsu',15);
+            $table->string('celular',10);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('direccion');
+            $table->date('fechaNac');
+            $table->boolean('rol');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
