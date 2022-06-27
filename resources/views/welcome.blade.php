ea28933d6afa6a9bd7c10e648df4f12f90af31c9
dflopezlo <dflopezlo@unal.edu.co>
2022-06-27 16:31:27 -0500

    se arreglan errores, también un módelo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,25 @@
-@extends('layouts.footer')
-@extends('layouts.navbar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INCORU APP</title>
+</head>
+<body>
+@include('layouts.navbar')
+<center>
+    <h1>AQUI VA LO QUE LLEVA ESTO EN ESPECIFICO</h1>
+</center>
+
+
+@include('layouts.footer')
+
+
+
+</body>
+</html>
+
+
+
+
